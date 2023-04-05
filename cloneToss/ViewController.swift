7cd82d5d48d4409f664aa0ad9a67c24b55a5f2f3
josu_shell <yoy07030@khu.ac.kr>
2023-04-05 17:35:20 +0900

    Initial Commit

---
+++ cloneToss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cloneToss
+//
+//  Created by JOSUEYEON on 2023/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
